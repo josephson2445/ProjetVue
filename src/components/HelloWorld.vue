3f9149c20d354ce6c9f737db4e657017a7b3293e
Josephson Juvenal <josephsonjuvenal39@gmail.com>
2026-06-06 09:46:08 +0300

    Initiation

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <label for="">
+    <input type="text" value="Entrez votre nom">
+    <input type="button" value="">
+  </label>
+</template>
+
+<style>
+
+</style>
